608d67aa7e679204e173bc1a28aa4b8bbf9e73ab
Jonnas Freire <70077642+jonnahfreire@users.noreply.github.com>
2020-11-24 20:07:29 -0300

    Cadastrando projetos pelo front

---
+++ front-end/src/App.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import Header from './components/Header'
+import api from './services/api'
+
+function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projeto').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
+  async function handleAddProject(){
+    const response = await api.post('projeto', {
+      title: `Projeto estático: ${Date.now()}`,
+      dev: "Jonnas"
+    })
+
+    const projeto = response.data
+
+    setProjects([...projects, projeto])
+  }
+
+  return(
+    <>
+        <Header title="React">
+          <ul>
+            {projects.map(project => (
+              <li key={project.id}>
+                {project.title}
+              </li>
+            ))}
+          </ul>
+        </Header>
+
+        <button type="button" onClick={handleAddProject}>
+          Adicionar Projeto
+        </button>
+
+    </>
+  )
+}
+
+export default App;
